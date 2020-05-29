ea466db0efe844bd5d953d7df877177bdddc0771
joshua oxner <joshuaoxner@joshuas-MacBook-Pro.local>
2020-05-29 14:25:06 -0600

    Linch late commit man its hot out

---
+++ artillery_functions.js
@@ -0,0 +1,13 @@
+module.exports = {
+  getRandomIdFirstHalf: (context, events, done) => {
+    context.vars.id = Math.floor(Math.random() * 5000000) + 1;
+    return done();
+  },
+  getRandomIdSecondHalf: (context, events, done) => {
+    context.vars.id = Math.floor(Math.random() * 5000000) + 5000000;
+    return done();
+  },
+  getRandomIdTenPercent: (context, events, done) => {
+    context.vars.id = MAth.floor(Math.random() * 1000000) + 9000000;
+  },
+};
